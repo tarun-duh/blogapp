0bdca910183804aff2f50a995f532e470a6f0d5c
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-02-19 14:29:53 +0530

    .

--- components/Login.jsx
+++ components/Login.jsx
@@ -44,11 +44,15 @@ export default function Login() {
                 setUserId(e.target.value);
                 const newvalue = e.target.value;
                 const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-                if (!emailRegex.test(newvalue)) {
-                  console.log(newvalue, emailRegex);
-                  setEmailError("please write a valid email address");
-                } else {
+                if (newvalue.length == 0) {
                   setEmailError("");
+                } else {
+                  if (!emailRegex.test(newvalue)) {
+                    console.log(newvalue, emailRegex);
+                    setEmailError("please write a valid email address");
+                  } else {
+                    setEmailError("");
+                  }
                 }
               }}
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
