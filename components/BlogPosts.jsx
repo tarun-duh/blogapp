366914dfcbef9e91db522075aee298c06889af88
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-04-17 15:28:58 +0530

    .

--- components/BlogPosts.jsx
+++ components/BlogPosts.jsx
@@ -87,25 +87,36 @@ export default function BlogPosts({
         <h2 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">
           {heading}
         </h2>
-        <p className="leading-relaxed mb-8">{para}</p>
+        <p className="leading-relaxed mb-8">
+          {readmore ? para : para.slice(0, 300)} {/* Show limited lines */}
+          {!readmore &&
+            para.length > 300 && ( // Show "Read More" if content exceeds limited lines
+              <button onClick={readmorefunc} className="text-indigo-500">
+                Read More
+              </button>
+            )}
+          {readmore &&
+            para.length > 300 && ( // Show "Show Less" button if content is expanded and exceeds limited lines
+              <button onClick={readmorefunc} className="text-indigo-500">
+                Show Less
+              </button>
+            )}
+        </p>
         <div className="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
-          <a
-            onClick={readmorefunc}
-            className="text-indigo-500 inline-flex items-center cursor-pointer"
-          >
-            Learn More
-            <svg
-              className="w-4 h-4 ml-2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M5 12h14"></path>
-              <path d="M12 5l7 7-7 7"></path>
-            </svg>
+          <a className="inline-flex items-center">
+            <img
+              alt="blog"
+              src={`${profile}`}
+              className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
+            />
+            <span className="flex-grow flex flex-col pl-4">
+              <span className="title-font font-medium text-gray-900">
+                {author}
+              </span>
+              <span className="text-gray-400 text-xs tracking-widest mt-0.5">
+                {date}
+              </span>
+            </span>
           </a>
           <div className="ml-auto flex justify-center items-center">
             <BiSolidLike
@@ -119,21 +130,6 @@ export default function BlogPosts({
             <p className="text-xl ml-3 ">{likeCount}</p>
           </div>
         </div>
-        <a className="inline-flex items-center">
-          <img
-            alt="blog"
-            src={`${profile}`}
-            className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center"
-          />
-          <span className="flex-grow flex flex-col pl-4">
-            <span className="title-font font-medium text-gray-900">
-              {author}
-            </span>
-            <span className="text-gray-400 text-xs tracking-widest mt-0.5">
-              {date}
-            </span>
-          </span>
-        </a>
       </div>
     </>
   );
